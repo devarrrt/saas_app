b32d10f6f40eb3630901307b83ed5d9aa600e872
Anastasia Burmakina <burmakina-1999@mail.ru>
2026-02-20 09:54:10 +0400

    create a navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
-import type { Metadata } from "next";
-import { Bricolage_Grotesque } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Bricolage_Grotesque } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/Navbar'
 
 const bricolage = Bricolage_Grotesque({
-  variable: "--font-bricolage",
-  subsets: ["latin"],
+  variable: '--font-bricolage',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Infocygan",
-  description: "Real-time AI Teaching Platform",
+  title: 'Infocygan',
+  description: 'Real-time AI Teaching Platform',
 };
 
 export default function RootLayout({
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>{children}</body>
+      <body className={`${bricolage.variable} antialiased`}>
+        <Navbar/>
+        {children}</body>
     </html>
   );
 }
