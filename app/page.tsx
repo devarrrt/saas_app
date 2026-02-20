3540a46d5a537cc3d71e7c62c9ababce2c1f1b05
Anastasia Burmakina <burmakina-1999@mail.ru>
2026-02-20 10:21:28 +0400

    create companions cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,43 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionCard from '@/components/CompanionCard';
+import CompanionsList from '@/components/CompanionsList';
+import CTA from '@/components/CTA';
 
 const Home = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'> hello </h1>
-      <Button>
-        hello test
-      </Button>
-    </div>
-  )
-}
+    <main>
+      <h1>Popular companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neural the Brainy Explorer"
+          topic="Neural Network of the Btain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the number wizard"
+          topic="Derivatives & integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary builder"
+          topic="English Literature"
+          subject="language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
